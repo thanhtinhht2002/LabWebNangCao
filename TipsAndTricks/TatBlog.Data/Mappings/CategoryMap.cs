cc71e61de923469adc88f477cc0381b22e396202
unknown <thanhtinhht2002@gmail.com>
2023-02-28 14:53:05 +0700

    lab01 first commit

---
+++ TipsAndTricks/TatBlog.Data/Mappings/CategoryMap.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TatBlog.Core.Entities;
+
+namespace TatBlog.Data.Mappings;
+
+public class CategoryMap: IEntityTypeConfiguration<Category>
+{
+    public void Configure(EntityTypeBuilder<Category>builder)
+    {
+        builder.ToTable("Authors");
+
+        builder.HasKey(p => p.Id);
+
+        builder.Property(p => p.Name)
+            .IsRequired()
+            .HasMaxLength(50);
+        builder.Property(p => p.Description)
+            .HasMaxLength(500);
+
+        builder.Property(p =>p.UrlSlug)
+            .IsRequired().HasMaxLength(50);
+
+        builder.Property(p=>p.ShowOnMenu).IsRequired()
+            .HasDefaultValue(false);
+
+          
+    }
+}
