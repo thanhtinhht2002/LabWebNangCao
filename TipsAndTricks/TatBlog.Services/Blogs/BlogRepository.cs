89bdeb6c7165c20afb8fb5d9d8b63dc892f15c5f
unknown <thanhtinhht2002@gmail.com>
2023-03-01 23:26:11 +0700

    update -3

--- TipsAndTricks/TatBlog.Services/Blogs/BlogRepository.cs
+++ TipsAndTricks/TatBlog.Services/Blogs/BlogRepository.cs
@@ -1,71 +1,118 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using TatBlog.Data.Contexts;
+using TatBlog.Core.Contracts;
+using TatBlog.Core.DTO;
 using TatBlog.Core.Entities;
-using Microsoft.EntityFrameworkCore;
+using TatBlog.Data.Contexts;
+using TatBlog.Services.Extensions;
 
-namespace TatBlog.Services.Blogs;
-
-public class BlogRepository : IBlogRepository
+namespace TatBlog.Services.Blogs
 {
-    private readonly IBlogRepository _context;
-    public BlogRepository(BlogRepository context)
+    public class BlogRepository : IBlogRepository
     {
-        _context = context;
-    }
-    //Tìm bài viết có tên định dạng 'Slug'
-    // và đc đăng vào tháng/ năm
-    public async Task<Post> GetPostAsync(
-        int year,
-        int month,
-        string slug,
-        CancellationToken cancellationToken=default)
-    {
-        IQueryable<Post> postsQuery = _context.Set<Post>()
+        private readonly BlogDbContext _context;
+        public BlogRepository(BlogDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Post> GetPostAsync(int year, int month, string slug, CancellationToken cancellationToken = default)
+        {
+            IQueryable<Post> postQuery = _context.Set<Post>()
             .Include(x => x.Category)
             .Include(x => x.Author);
+            if (year > 0)
+            {
+                postQuery = postQuery.Where(x => x.PostedDate.Year == year);
+            }
+            if (month > 0)
+            {
+                postQuery = postQuery.Where(x => x.PostedDate.Month == month);
+            }
+            if (!string.IsNullOrWhiteSpace(slug))
+            {
+                postQuery = postQuery.Where(x => x.UrlSlug == slug);
+            }
+            return await postQuery.FirstOrDefaultAsync(cancellationToken);
 
-        if (year>0)
-        {
-            postsQuery = postsQuery.Where(x => x.PostedDate.Year == year);
         }
-        if (month > 0)
-        {
-            postsQuery = postsQuery.Where(x => x.PostedDate.Month == month);
-        }
-        if (!string.IsNullOrWhiteSpace(slug))
-        {
-            postsQuery = postsQuery.Where(x=>x.UrlSlug==slug);
-        }
-        return await postsQuery.FirstOrDefaultAsync(cancellationToken);
-    }
 
-    public async Task<IList<Post>> GetPopularArticlesAsync(
-        int numPosts, CancellationToken cancellationToken = default)
-    {
-        return await _context.Set<Post>()  
-            .Include(x => x.Author)
-            .Include(x => x.Category)
-            .OrderbyDescanding(p=>p.ViewCount)
-            .Take(numPosts)
-            .ToListAsync(cancellationToken);
-    }
+        public async Task<IList<Post>> GetPopularArticlesAsync(
+            int numPosts,
+            CancellationToken cancellationToken = default)
+        {
+            return await _context.Set<Post>()
+                .Include(x => x.Author)
+                .Include(x => x.Category)
+                .OrderByDescending(p => p.ViewCount)
+                .Take(numPosts)
+                .ToListAsync(cancellationToken);
+        }
 
-    public Task<bool> IsPostSlugExisteedAsync(
-        int postId,
-        string slug,
+        public async Task<bool> IsPostSlugExistedAsync(
+            int postId,
+            string slug,
+            CancellationToken cancellationToken = default)
+        {
+            return await _context.Set<Post>()
+                .AnyAsync(x => x.Id != postId && x.UrlSlug == slug, cancellationToken);
+        }
+        public async Task IncreaseViewCountAsync(
+            int postId,
+            CancellationToken cancellationToken = default)
+        {
+            await _context.Set<Post>()
+                .Where(x => x.Id == postId)
+                .ExecuteUpdateAsync(p => p.SetProperty(x => x.ViewCount, x => x.ViewCount + 1), cancellationToken);
+        }
+
+
+
+        public async Task<IList<CategoryItem>> GetCategoriesAsync(bool showOnMenu = false, CancellationToken cancellationToken = default)
+        {
+            IQueryable<Category> categories = _context.Set<Category>();
+            if (showOnMenu)
+            {
+                categories = categories.Where(x => x.ShowOnMenu);
+            }
+            return await categories
+                .OrderBy(x => x.Name)
+                .Select(x => new CategoryItem()
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    UrlSlug = x.UrlSlug,
+                    Description = x.Description,
+                    ShowOnMenu = x.ShowOnMenu,
+                    PostCount = x.Posts.Count(p => p.Published)
+                })
+                .ToListAsync(cancellationToken);
+        }
+        public async Task<IPagedList<TagItem>> GetPagedTagsAsync(
+        IPagingParams pagingParams,
         CancellationToken cancellationToken = default)
-    {
-        throw new NotImplementedException();
-    }
+        {
+            var tagQuery = _context.Set<Tag>()
+                .Select(x => new TagItem()
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    UrlSlug = x.UrlSlug,
+                    Description = x.Description,
+                    PostCount = x.Posts.Count(p => p.Published)
+                });
 
-    public Task IncreaseViewCountAsync(
-        int postId,
-        CancellationToken cancellationToken = default)
-    {
-        throw new NotImplementedException();
-    }
+            return await tagQuery
+                .ToPagedListAsync(pagingParams, cancellationToken);
+        }
 
-   
+        public Task<bool> IsPostSlugExisteedAsync(int postId, string slug, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
--- TipsAndTricks/TatBlog.WinApp/Program.cs
+++ TipsAndTricks/TatBlog.WinApp/Program.cs
@@ -1,30 +1,61 @@
 ﻿using TatBlog.Core.Entities;
 using TatBlog.Data.Contexts;
 using TatBlog.Data.Seeders;
+using TatBlog.Services.Blogs;
+using TatBlog.WinApp;
 
 var context = new BlogDbContext();
 
-var posts = context.Posts
-    .Where(p => p.Published)
-    .OrderBy(p => p.Title)
-    .Select(p => new
-    {
-        Id = p.Id,
-        Title = p.Title,
-        ViewCount = p.ViewCount,
-        PostedDate = p.PostedDate,
-        Author = p.Author.FullName,
-        Category = p.Category.Name,
-    }).ToList();
-//Xuat ds bai viet
+var seeder = new DataSeeder(context);
+seeder.Initialize();
+// tao doi tuong 
+IBlogRepository blogRepo = new BlogRepository(context);
 
-foreach (var post in posts)
+var pagingParams = new PagingParams
 {
-    Console.WriteLine("ID       :{0}",post.Id);
-    Console.WriteLine("Title    :{0}", post.Title);
-    Console.WriteLine("View     :{0}", post.ViewCount);
-    Console.WriteLine("Date     :{0:MM/dd/yyyy}", post.PostedDate);
-    Console.WriteLine("Author   :{0}", post.Author);
-    Console.WriteLine("Category :{0}", post.Category);
-    Console.WriteLine("".PadRight(80,'-'));
+    PageNumber = 1,
+    PageSize = 5,
+    SortColumn = "Name",
+    SortOrder = "DESC"
+};
+var tagsList = await blogRepo.GetPagedTagsAsync(pagingParams);
+
+Console.WriteLine("{0,-5}{1,-50}{2,10}", "ID", "Name", "Count");
+
+foreach (var item in tagsList)
+{
+    Console.WriteLine("{0,-5}{1,-50}{2,10}", item.Id, item.Name, item.PostCount);
 }
+
+
+
+
+
+
+
+
+
+//var posts = context.Posts
+//    .Where(p => p.Published)
+//    .OrderBy(p => p.Title)
+//    .Select(p => new
+//    {
+//        Id = p.Id,
+//        Title = p.Title,
+//        ViewCount = p.ViewCount,
+//        PostedDate = p.PostedDate,
+//        Author = p.Author.FullName,
+//        Category = p.Category.Name,
+//    }).ToList();
+////Xuat ds bai viet
+
+//foreach (var post in posts)
+//{
+//    Console.WriteLine("ID       :{0}",post.Id);
+//    Console.WriteLine("Title    :{0}", post.Title);
+//    Console.WriteLine("View     :{0}", post.ViewCount);
+//    Console.WriteLine("Date     :{0:MM/dd/yyyy}", post.PostedDate);
+//    Console.WriteLine("Author   :{0}", post.Author);
+//    Console.WriteLine("Category :{0}", post.Category);
+//    Console.WriteLine("".PadRight(80,'-'));
+//}
